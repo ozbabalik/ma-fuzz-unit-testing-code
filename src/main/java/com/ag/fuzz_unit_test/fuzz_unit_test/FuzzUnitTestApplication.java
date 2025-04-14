20c6b3739d576e76e6891320c0b8ac902eb11650
Arapsih Güngör <arapsih.guengoer@gmail.com>
2025-04-14 16:08:03 +0200

    init

---
+++ src/main/java/com/ag/fuzz_unit_test/fuzz_unit_test/FuzzUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.ag.fuzz_unit_test.fuzz_unit_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzzUnitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ag/fuzz_unit_test/fuzz_unit_test/FuzzUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.fuzz_unit_test.fuzz_unit_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzzUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
